e512c7cb06e54a51b59aa408dd5b1d51609899a4
Yoyo <yoyo@YoyodeMacBook-Pro.local>
2022-10-07 20:59:55 +1100

    admin

--- comp9900/comp9900/models.py
+++ comp9900/comp9900/models.py
@@ -1,48 +1,57 @@
 from django.db import models
+from django.contrib import admin
 
 
 class Needs(models.Model):
     needs_name = models.CharField(max_length=255, unique=True, primary_key=True)
 
     class Meta:
-        ordering = ['id']
+        verbose_name = "needs"
 
     def __str__(self):
         return self.needs_name
 
 
-class Event(models.Model):
-    description = models.TextField(blank=False)
-    start_date = models.DateField(blank=False)
-    end_date = models.DateField(blank=False)
-
-
 class Charity(models.Model):
     charity_name = models.CharField(max_length=255, unique=True, blank=False)
-    email = models.CharField(unique=True, blank=False)
-    needs = models.ForeignKey(Needs, blank=True)
+    email = models.CharField(unique=True, blank=False, max_length=255)
+    needs = models.ManyToManyField(Needs, blank=True)
     description = models.TextField(blank=False)
-    event = models.ForeignKey(Event, blank=True)
     password = models.CharField(max_length=255, blank=True)
 
     class Meta:
-        ordering = ['id']
+        verbose_name = "charity"
 
     def __str__(self):
         return self.charity_name
 
 
 class Sponsor(models.Model):
-    sponsor_name = charity_name = models.CharField(max_length=255, unique=False, blank=False)
-    email = models.CharField(unique=True, blank=False)
+    sponsor_name = models.CharField(max_length=255, unique=False, blank=False)
+    email = models.CharField(unique=True, blank=False, max_length=255)
     description = models.TextField(blank=False)
-    needs = models.ForeignKey(Needs, blank=True)
+    needs = models.ManyToManyField(Needs, blank=True)
     website_link = models.CharField(max_length=255, blank=True)
-    follows = models.ForeignKey(Charity, blank=True)
+    follows = models.ManyToManyField(Charity, blank=True)
     password = models.CharField(max_length=255, blank=True)
 
     class Meta:
-        ordering = ['id']
+        verbose_name = "sponsor"
 
     def __str__(self):
         return self.sponsor_name
+
+
+class Event(models.Model):
+    description = models.TextField(blank=False)
+    start_date = models.DateField(blank=False)
+    end_date = models.DateField(blank=False)
+    Charity = models.ForeignKey(Charity, blank=False, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "event"
+
+
+@admin.register(Charity)
+class CharityAdmin(admin.ModelAdmin):
+    list_display = ("id", "charity_name", "email", "description", "password")
--- comp9900/comp9900/views.py
+++ comp9900/comp9900/views.py
@@ -1,10 +1,19 @@
+import email
+
 from django.http import HttpResponse
 from rest_framework import serializers
 from rest_framework.generics import GenericAPIView
-import models
+from . import models
+from rest_framework.response import Response
 
 
 class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    charity = serializers.BooleanField()
+    sponsor = serializers.BooleanField()
+
+class RegisterSerializer(serializers.Serializer):
     email = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
     confirm_password = serializers.CharField(required=True)
@@ -15,6 +24,25 @@ class LoginSerializer(serializers.Serializer):
 def login(GenericAPIView):
     def post(self, request):
         login_data = LoginSerializer(data=request.data)
-        if login_data.charity:
+        if login_data.charity and not login_data.sponsor:
+            charity = models.Charity.objects.get(email=email)
+            if not charity:
+                return Response(data={'message': 'The email does not exists'})
+            elif charity.password == login_data.password:
+                return Response(data={'message': 'success'})
+            else:
+                return Response(data={'message': 'Wrong password or email'})
+        elif login_data.sponsor and not login_data.charity:
+            sponsor = models.Sponsor.objects.get(email=email)
+            if not sponsor:
+                return Response(data={'message': 'The email does not exists'})
+            elif sponsor.password == login_data.password:
+                return Response(data={'message': 'success'})
+            else:
+                return Response(data={'message': 'Wrong password or email'})
+        else:
+            return Response(data={'message': 'You should be a charity or a sponsor'})
+
+# def register(GenericAPIView):
 
 
